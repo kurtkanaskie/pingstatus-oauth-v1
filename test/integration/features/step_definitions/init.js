5f47e212aa8fddd4dc48ec6d441c62fdf10437f1
Kurt Googler Kanaskie <kurtkanaskie@google.com>
2017-12-18 10:49:35 -0500

    Cleaned up and combined init.js and factory.js

--- test/integration/features/step_definitions/init.js
+++ test/integration/features/step_definitions/init.js
@@ -1,30 +1,32 @@
 /* jshint node:true */
 'use strict';
 
-var factory = require('./factory.js');
+var apickli = require('apickli');
 var config = require('../../test-config.json');
 var apps = require('../../devAppKeys.json');
+
+console.log('apiconfig: [' + config.apiconfig.domain + ', ' + config.apiconfig.basepath + ']');
 
 module.exports = function() {
     // cleanup before every scenario
     this.Before(function(scenario, callback) {
-        this.apickli = factory.getNewApickliInstance();
+        this.apickli = new apickli.Apickli('https', config.apiconfig.domain + config.apiconfig.basepath);
+        
         this.apickli.storeValueInScenarioScope("apiproxy", config.apiconfig.apiproxy);
         this.apickli.storeValueInScenarioScope("basepath", config.apiconfig.basepath);
         getCredsFromExport(config.apiconfig.app, config.apiconfig.product);
-        this.apickli.storeValueInScenarioScope("clientId", creds.clientId);
-        this.apickli.storeValueInScenarioScope("clientSecret", creds.clientSecret);
-        this.apickli.storeValueInScenarioScope("invalidClientSecret", config.apiconfig.invalidClientSecret);
+        // console.log( "INIT CREDS: " + creds.clientId + " " + creds.clientSecret);
+        this.apickli.storeValueInScenarioScope("clientId", keys.clientId);
+        this.apickli.storeValueInScenarioScope("clientSecret", keys.clientSecret);
         this.apickli.storeValueInScenarioScope("oauthDomain", config.apiconfig.oauthDomain );
         this.apickli.storeValueInScenarioScope("oauthBasepath", config.apiconfig.oauthBasepath);
         this.apickli.storeValueInScenarioScope("userUsername", config.apiconfig.userUsername);
         this.apickli.storeValueInScenarioScope("userPassword", config.apiconfig.userPassword);
-        console.log( "INIT CREDS: " + creds.clientId + " " + creds.clientSecret);
         callback();
     });
 };
 
-var creds = {};
+var keys = {};
 
 // Just take the first match
 function getCredsFromExport(appName, productName){
@@ -35,8 +37,8 @@ function getCredsFromExport(appName, productName){
         var products = credentials[credential].apiProducts;
         for(var product in products){
           if(products[product].apiproduct === productName){
-            creds.clientId = credentials[credential].consumerKey;
-            creds.clientSecret = credentials[credential].consumerSecret;
+            keys.clientId = credentials[credential].consumerKey;
+            keys.clientSecret = credentials[credential].consumerSecret;
           }
           break;
         }
